0389da4762d1491e8ea5512cb8710d05e797d59f
Andrew Tasker <atasker2@huskers.unl.edu>
2020-08-24 17:57:25 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Andrew Tasker
+ *         cbourke@cse.unl.edu
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Andrew Tasker\n");
+  printf("Finance and computer science\n");
+
+  return 0;
+}
